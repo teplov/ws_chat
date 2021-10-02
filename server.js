cc3ad76012892ecc3e687900fc3625ac4ef0caa3
Roman Teplov <teplov@gmail.com>
2021-10-02 14:08:26 +0300

    Chat first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const WebSocket = require('ws');
+
+const wsServer = new WebSocket.Server({ port: 9000 });
+
+const onConnect = (wsClient) => {
+    console.log('Новый пользователь');
+    // отправка приветственного сообщения клиенту
+    wsClient.send('Привет');
+    wsClient.on('message', (message) => {
+        try {
+            // сообщение пришло текстом, нужно конвертировать в JSON-формат
+            const jsonMessage = JSON.parse(message);
+            switch (jsonMessage.action) {
+                case 'client_send_message':
+                    console.log(jsonMessage.data);
+                    break;
+                default:
+                    console.log('Неизвестная команда');
+                    break;
+            }
+        } catch (error) {
+            console.log('Ошибка', error);
+        }
+    });
+    wsClient.on('close', () => {
+        // отправка уведомления в консоль
+        console.log('Пользователь отключился');
+    });
+};
+
+wsServer.on('connection', onConnect);
+
+console.log('Сервер запущен на 9000 порту');
